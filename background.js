f75164c7d5f4f2aa477107533328bbcf561993c7
Joshua Britt <joshua@britt.email>
2020-02-13 02:04:32 -0600

    Set default extension options.

--- background.js
+++ background.js
@@ -1,3 +1,16 @@
 chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.create({'url': 'chrome://newtab'})
 });
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.local.set({
+    'options': {
+      'clocks': [{
+        'timeZone': 'Local',
+        'label': 'Local',
+      }],
+      'hour24': false,
+      'labels': true
+    }
+  });
+});
