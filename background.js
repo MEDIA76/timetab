8259924639fd1a60ec6e38e00b28bcd7105b0ee2
Joshua Britt <joshua@britt.email>
2020-02-18 12:24:53 -0600

    Removed hanging comma.

--- background.js
+++ background.js
@@ -10,7 +10,7 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({
     'clocks': [{
       'timeZone': 'Local',
-      'label': 'Local',
+      'label': 'Local'
     }],
     'settings': {
       'hour24': false,
